354f64feb0231d8a78bbcfaac7623ba9c6508ed4
Hal Hildebrand <hal.hildebrand@gmail.com>
2012-09-18 11:05:22 -0700

    initial cut

---
+++ src/main/java/com/hellblazer/dnsextd/DnsExtd.java
@@ -0,0 +1,77 @@
+/** 
+ * (C) Copyright 2012 Hal Hildebrand, all rights reserved.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.hellblazer.dnsextd;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hellblazer.pinkie.CommunicationsHandler;
+import com.hellblazer.pinkie.CommunicationsHandlerFactory;
+import com.hellblazer.pinkie.ServerSocketChannelHandler;
+import com.hellblazer.pinkie.SocketOptions;
+
+/**
+ * A daemon process which provides extension functionality for <a
+ * href="files.dns-sd.org/draft-sekar-dns-llq.txt">DNS long lived queries</a>
+ * and <a href="files.dns-sd.org/draft-sekar-dns-ul.txt">Dynamic DNS update
+ * lease</a> for DNS Service Discovery clients.
+ * 
+ * @author hhildebrand
+ * 
+ */
+public class DnsExtd {
+    @SuppressWarnings("unused")
+    private final static Logger              log     = LoggerFactory.getLogger(DnsExtd.class);
+
+    private final ServerSocketChannelHandler handler;
+    private final AtomicBoolean              running = new AtomicBoolean();
+
+    public DnsExtd(InetSocketAddress address, SocketOptions socketOptions,
+                   ExecutorService commsExec) throws IOException {
+
+        handler = new ServerSocketChannelHandler(
+                                                 "DNS EXTD Daemon",
+                                                 socketOptions,
+                                                 address,
+                                                 commsExec,
+                                                 new CommunicationsHandlerFactory() {
+                                                     @Override
+                                                     public CommunicationsHandler createCommunicationsHandler(SocketChannel channel) {
+                                                         return new LlqSession();
+                                                     }
+
+                                                 });
+    }
+
+    public void start() {
+        if (running.compareAndSet(false, true)) {
+            handler.start();
+        }
+    }
+
+    public void terminate() {
+        if (running.compareAndSet(true, false)) {
+            handler.terminate();
+        }
+    }
+}
